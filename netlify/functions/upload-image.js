3f7332f6e27dc08d9410472faa83cd25519bd626
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-03-21 09:14:41 +0530

    added edge fun to upload image

---
+++ netlify/functions/upload-image.js
@@ -0,0 +1,66 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseClient = createClient(
+    process.env.SUPABASE_DATABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
+export default async (request) => {
+
+    if (request.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 204,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'POST, OPTIONS'
+            }
+        });
+    }
+
+    try {
+        const formData = await request.formData();
+
+        const image = formData.get("image");
+        const filePath = formData.get("filePath");
+
+        if (!image) {
+            return new Response(
+                JSON.stringify({ success: false, error: "Image required" }),
+                { status: 400 }
+            );
+        }
+
+        const { error: uploadError } = await supabaseClient
+            .storage
+            .from("product_images")
+            .upload(filePath, image, {
+                cacheControl: "3600",
+                upsert: true
+            });
+
+        if (uploadError) {
+            return new Response(
+                JSON.stringify({ success: false, error: uploadError.message }),
+                { status: 500 }
+            );
+        }
+
+        return new Response(
+            JSON.stringify({ success: true, publicUrl: `https://kcksejyyjfgpcdmgtzrc.supabase.co/storage/v1/object/public/product_images/${filePath}` }),
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        );
+
+    } catch (err) {
+        return new Response(
+            JSON.stringify({ success: false, error: err.message }),
+            { status: 500 }
+        );
+    }
+};
