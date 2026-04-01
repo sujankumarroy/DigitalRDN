2f6048ecb072de3d3707f94f55fe44501db6bcba
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-01 09:47:57 +0530

    wip: compiled script.ts

---
+++ dist/script.d.ts
@@ -0,0 +1,14 @@
+declare const root_path = "https://kcksejyyjfgpcdmgtzrc.supabase.co/storage/v1/object/public/product_images/";
+declare function loadProducts(): Promise<void>;
+declare function openPopup(): void;
+declare function closePopup(): void;
+declare function scrollToProducts(): void;
+declare function sendTID(): void;
+declare function filterProducts(): void;
+declare function toggleProduct(button: HTMLElement, name: string, price: number, type: string): void;
+declare function payNow(): void;
+declare function downloadQR(): void;
+declare function updateBuyListDisplay(): void;
+declare function clearBuyList(): void;
+declare function shareWhatsAppList(): void;
+//# sourceMappingURL=script.d.ts.map
