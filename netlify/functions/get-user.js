3f6a4f56225d1ad12a91222f5316d83269a366d3
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-06 22:09:10 +0530

    implemented get-user api

---
+++ netlify/functions/get-user.js
@@ -0,0 +1,57 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseClient = createClient(process.env.SUPABASE_DATABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+export default async (request) => {
+
+    if (request.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 204,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+            }
+        });
+    }
+
+    try {
+        const { email, password } = await request.json();
+
+        if (!email || !password) {
+            return new Response(
+                JSON.stringify({success: false, error: "Invalid email and password"}),
+                { status: 500, headers: defaultHeader() }
+            );
+        }
+
+        const { data, error } = await supabaseClient
+            .from("users")
+            .select("*");
+
+        if (error) {
+            return new Response(
+                JSON.stringify({ success: false, error: error.message }),
+                { status: 500, headers: defaultHeader() }
+            );
+        }
+
+        return new Response(
+            JSON.stringify({ success: true, data: data }),
+            { status: 200, headers: defaultHeader() }
+        );
+
+    } catch (err) {
+        return new Response(
+            JSON.stringify({ success: false, error: err.message }),
+            { status: 500, headers: defaultHeader() }
+        );
+    }
+};
+
+function defaultHeader() {
+    return {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    }
+}
