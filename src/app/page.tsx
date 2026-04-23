dee5431d3b61f7a79550d76249835aabcf692ee3
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-23 17:35:30 +0530

    added Buy list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import BuyList from "@/components/BuyList";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Search from "@/components/Search";
 import React from "react";
 
 function Home() {
   return (
     <div>
       <Header />
-      <div className="h-screen"></div>
+      <div className="h-screen pt-20 max-w-200 m-auto">
+        <Search />
+        <BuyList />
+      </div>
       <Footer />
     </div>
   );
