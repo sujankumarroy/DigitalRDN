7a614391dcf736094171edf4652db0c7047815d0
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-26 15:46:45 +0530

    added on click listener in back putton and profile icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import React from "react";
 import logo from "../../public/icons/android-chrome-512x512.png";
 import demoAvatar from "../../public/images/demo-avater.jpg";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 function Header() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <header>
       <div className="fixed w-screen bg-green-900 flex items-center justify-between gap-5 p-2">
         <div className="flex h-12 w-12 items-center justify-center rounded-[50%]">
-          <p className="text-4xl text-white p-0 m-0 relative right-0">←</p>
+          <p
+            className={`text-4xl text-white p-0 m-0 relative right-0 ${pathname === "/" ? "hidden" : ""}`}
+            onClick={() => router.back()}
+          >
+            ←
+          </p>
         </div>
         <div className="flex items-center justify-center gap-5 p-2">
           <Image src={logo} alt="rdn logo" width={50} height={50} />
@@ -20,6 +31,7 @@ function Header() {
           alt="profile"
           src={demoAvatar}
           className="rounded-[50%]"
+          onClick={() => router.push("/profile")}
         />
       </div>
     </header>
