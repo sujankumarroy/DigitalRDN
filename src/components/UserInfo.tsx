9e63ca8f132e6aefded9efc76b9153ec059c486b
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-26 20:29:57 +0530

    managed state for user info

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import demoAvatar from "../../public/images/demo-avater.jpg";
+"use client";
+
+import React, { use, useEffect, useState } from "react";
 import Image from "next/image";
 
 function UserInfo() {
+  const [name, setName] = useState("Unknown");
+  const [email, setEmail] = useState("name@example.com");
+  const [avater, setAvatar] = useState("/images/demo-avater.jpg");
+
   return (
     <section className="bg-green-200 p-5 m-2.5 rounded-2xl hover:bg-green-300 flex items-center justify-around">
       <Image
         width={150}
         height={150}
         className="rounded-[50%]"
-        src={demoAvatar}
+        src={avater}
         alt="avater"
       />
       <div>
-        <p className="p-1 text-3xl">Unknown</p>
+        <p className="p-1 text-3xl">{name}</p>
         <p className="p-1 text-xl" id="user-email">
-          name@example.com
-        </p>
-        <p className="p-1 text-xl" id="user-phone">
-          +91 0000000000
+          {email}
         </p>
       </div>
     </section>
