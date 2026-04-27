ee564da35ef30c2e2907064e811ebb59a6dc0a87
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-27 13:46:26 +0530

    implemented signin api

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,45 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
+
+  async function singIn() {
+    try {
+      if (!email || !password) {
+        alert("Failed to login!\nEnter Email and Password properly.");
+        return;
+      }
+      let res = await fetch(
+        "https://digitalrdn.netlify.app/.netlify/functions/get-user",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+      if (!res.ok) console.error(`Failed to Fetch. error: ${res.status}`);
+      const { data, error } = await res.json();
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (!data[0]) {
+        console.log("No credential found with your email and password");
+        return;
+      }
+      localStorage.setItem("rdn-user", JSON.stringify(data[0]));
+      alert("succesfully signed in");
+      router.push("/");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="min-h-100 pt-20 p-5 max-w-200 m-auto h-screen flex justify-center items-center text-center">
       <form className="w-100 border rounded-3xl p-3 px-10 bg-green-200">
@@ -14,6 +49,7 @@ function SignIn() {
             type="email"
             placeholder="Email"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col p-1">
@@ -22,6 +58,7 @@ function SignIn() {
             type="password"
             placeholder="Password"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <p onClick={() => router.push("/signup")}>
@@ -31,8 +68,8 @@ function SignIn() {
         <button
           type="submit"
           onClick={(e) => {
-            e.preventDefault;
-            console.log(e);
+            e.preventDefault();
+            singIn();
           }}
           className="bg-green-400 hover:bg-green-600 rounded-lg w-full mt-5 p-2 "
         >
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordR, setPasswordR] = useState("");
   const router = useRouter();
   return (
     <div className="min-h-100 pt-20 p-5 max-w-200 m-auto h-screen flex justify-center items-center text-center">
@@ -14,6 +18,7 @@ function SignUp() {
             type="text"
             placeholder="Name"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="flex flex-col p-1">
@@ -22,6 +27,7 @@ function SignUp() {
             type="email"
             placeholder="Email"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col p-1">
@@ -30,6 +36,7 @@ function SignUp() {
             type="password"
             placeholder="Password"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="flex flex-col p-1">
@@ -38,6 +45,7 @@ function SignUp() {
             type="password"
             placeholder="Confirm Password"
             className="border rounded-sm p-0.5 m-0.5"
+            onChange={(e) => setPasswordR(e.target.value)}
           />
         </div>
         <p onClick={() => router.push("/signin")}>
@@ -47,8 +55,8 @@ function SignUp() {
         <button
           type="submit"
           onClick={(e) => {
-            e.preventDefault;
-            console.log(e);
+            e.preventDefault();
+            console.log({ name, email, password, passwordR });
           }}
           className="bg-green-400 hover:bg-green-600 rounded-lg w-full mt-5 p-2 "
         >
