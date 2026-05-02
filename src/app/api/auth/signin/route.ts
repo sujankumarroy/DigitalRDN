493ec2cc32ab4f9b338c22ac20142c17caa954d7
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-02 12:33:18 +0530

    upgraded signin route

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,35 +1,45 @@
 import connectDb from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
   try {
     const { email, password } = await req.json();
 
-    if (!email || !password)
+    if (!email || !password) {
       return NextResponse.json(
-        { error: "email and password are required" },
-        { status: 401 },
+        { error: "Email and password required" },
+        { status: 400 },
       );
+    }
 
     const db = await connectDb();
 
-    const { data, error } = await db
+    const { data: user, error } = await db
       .from("users")
       .select("*")
       .eq("email", email)
-      .eq("password", password);
+      .single();
 
-    if (error) return NextResponse.json(error, { status: 500 });
-
-    if (!data || data.length === 0) {
+    if (error || !user) {
       return NextResponse.json(
         { error: "Invalid credentials" },
         { status: 401 },
       );
     }
 
-    return NextResponse.json(data[0], { status: 200 });
+    const isMatch = await bcrypt.compare(password, user.password);
+    delete user.password;
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 },
+      );
+    }
+
+    return NextResponse.json(user, { status: 200 });
   } catch (error) {
-    return NextResponse.json(error, { status: 500 });
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
