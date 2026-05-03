034c8de15ae82030efef1d2421468bd7ece68234
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-03 23:55:14 +0530

    added authorization based on jwt token to update product

--- src/app/api/product/update-product/route.tsx
+++ src/app/api/product/update-product/route.tsx
@@ -1,9 +1,35 @@
 import connectDb from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export async function POST(req: NextRequest) {
   try {
+    const token = req.cookies.get("token")?.value;
+    if (!token)
+      return NextResponse.json(
+        { error: { message: "jwt not found" } },
+        { status: 500 },
+      );
+    const jwtSecret = process.env.JWT_SECRET as string;
+    const payload = jwt.verify(token, jwtSecret) as JwtPayload & {
+      user_id: string;
+    };
+
     const db = await connectDb();
+
+    const { data: user, error: err } = await db
+      .from("users")
+      .select("*")
+      .eq("id", payload.user_id)
+      .single();
+
+    if (err) return NextResponse.json({ error: err }, { status: 500 });
+    if (Object.keys(user).length === 0) return NextResponse.json({});
+    if (user.role !== "admin")
+      return NextResponse.json({
+        error: { message: "not allowed to update product" },
+      });
+
     const {
       id,
       key,
