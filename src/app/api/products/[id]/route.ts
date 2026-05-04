dd5733a211e83330e083685f9f3c511fce469ab2
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-04 23:49:06 +0530

    implemented put method to update product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,6 +1,6 @@
 import connectDb from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export async function GET(
   req: NextRequest,
@@ -45,6 +45,83 @@ export async function GET(
     }
 
     return NextResponse.json({ data: product }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    const token = req.cookies.get("token")?.value;
+
+    if (!token)
+      return NextResponse.json(
+        { error: { message: "jwt not found" } },
+        { status: 500 },
+      );
+
+    const jwtSecret = process.env.JWT_SECRET as string;
+    const payload = jwt.verify(token, jwtSecret) as JwtPayload & {
+      user_id: string;
+    };
+
+    const db = await connectDb();
+    const { data: user, error: err } = await db
+      .from("users")
+      .select("*")
+      .eq("id", payload.user_id)
+      .maybeSingle();
+
+    if (err) return NextResponse.json({ error: err }, { status: 500 });
+    if (!user) {
+      return NextResponse.json(
+        { error: { message: "unauthorized user" } },
+        { status: 400 },
+      );
+    }
+
+    if (user.role !== "admin") {
+      return NextResponse.json(
+        { error: { message: "not allowed to update product" } },
+        { status: 403 },
+      );
+    }
+
+    const { id } = await context.params;
+    const { name, price, type, unit, stock_quantity, min_stock } =
+      await req.json();
+
+    if (!name || !price || !unit || !type) {
+      return NextResponse.json(
+        { error: { message: "missing name, price, type or unit." } },
+        { status: 400 },
+      );
+    }
+
+    let product: ProductType = {
+      name,
+      price,
+      type,
+      unit,
+      stock_quantity,
+      min_stock,
+    };
+
+    const { data, error } = await db
+      .from("products")
+      .update(product)
+      .eq("id", id)
+      .select()
+      .single();
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 502 });
+    }
+
+    return NextResponse.json({ data }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
